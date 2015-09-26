f43fd90bba1d05e10a421b385e914131c67148ba
osxi <osxi@users.noreply.github.com>
2015-09-26 19:38:21 +0000

    add remaining crud for chirps

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -6,8 +6,13 @@ class ChirpsController < ApplicationController
   def new
     @chirp = Chirp.new
   end
+
+  def destroy
+    Chirp.find(params[:id]).destroy
+    redirect_to chirps_path
+  end
   
-def create
+  def create
     @chirp = Chirp.new(params[:chirp].permit(:author, :body))
     if @chirp.save
       redirect_to chirp_path(@chirp)
@@ -17,6 +22,17 @@ def create
   end
 
   def edit
+    @chirp = Chirp.find(params[:id])
+  end
+
+  def update
+    @chirp = Chirp.find(params[:id])
+
+    if @chirp.update(params[:chirp].permit(:author, :body))
+      redirect_to chirp_path(@chirp)
+    else
+      render 'edit'
+    end
   end
 
   def show
